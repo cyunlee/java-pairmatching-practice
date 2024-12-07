3e2b099e5e2604e0cec810520fb1f0c85b02cafb
cyunlee <siyunkr@gmail.com>
2024-12-07 22:10:36 +0900

    feat: OutputView 기본 기능 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -3,8 +3,30 @@ package pairmatching.view;
 public class OutputView {
     private final static String ERROR = "[ERROR] ";
 
-    public void printPrompt() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    public void printFunctionChoicePrompt() {
+        System.out.println("기능을 선택하세요.");
+        System.out.println("1. 페어 매칭");
+        System.out.println("2. 페어 조회");
+        System.out.println("3. 페어 초기화");
+        System.out.println("Q. 종료");
+    }
+
+    public void printCourseLevelMission() {
+        System.out.println("#############################################");
+        System.out.println("과정: 백엔드 | 프론트엔드");
+        System.out.println("미션:");
+        System.out.println("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임");
+        System.out.println("  - 레벨2: 장바구니 | 결제 | 지하철노선도");
+        System.out.println("  - 레벨3:");
+        System.out.println("  - 레벨4: 성능개선 | 배포");
+        System.out.println("  - 레벨5:");
+        System.out.println("############################################");
+        System.out.println("과정, 레벨, 미션을 선택하세요.");
+        System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+    }
+
+    public void printMatchResult() {
+        System.out.println("페어 매칭 결과입니다.");
     }
 
     public void printError(String message) {
