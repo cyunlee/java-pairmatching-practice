2f85303699ed15aef75932921dc220b72966a07c
cyunlee <siyunkr@gmail.com>
2024-12-07 22:04:44 +0900

    feat: 도메인들 속성 및 메서드 추가

--- src/main/java/pairmatching/domain/Crews.java
+++ src/main/java/pairmatching/domain/Crews.java
@@ -1,4 +1,19 @@
 package pairmatching.domain;
 
-public class Crews {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Crews implements Iterable<Crew> {
+    private List<Crew> crews = new ArrayList<>();
+
+    public List<Crew> addCrew(Crew crew) {
+        crews.add(crew);
+        return crews;
+    }
+
+    @Override
+    public Iterator<Crew> iterator() {
+        return crews.iterator();
+    }
 }
--- src/main/java/pairmatching/exception/InputValidator.java
+++ src/main/java/pairmatching/exception/InputValidator.java
@@ -1,5 +1,7 @@
 package pairmatching.exception;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
     private final static String INPUT_INVALID_ERROR = "입력 값은 빈 값이 될 수 없습니다. 다시 입력해주세요.";
     private final static String INPUT_RANGE_INVALID_ERROR = "입력 값은 숫자만 될 수 있습니다. 다시 입력해주세요.";
